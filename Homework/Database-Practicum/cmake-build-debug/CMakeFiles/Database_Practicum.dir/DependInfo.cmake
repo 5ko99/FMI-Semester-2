a234367a25eb61f430515d119bed1c69084336b6
5ko99 <petko.krasimirov99@gmail.com>
2019-04-30 15:10:55 +0300

    Fixed bugs and finished update function

---
+++ Homework/Database-Practicum/cmake-build-debug/CMakeFiles/Database_Practicum.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/petko/Workspace/FMI-Semester-2/Homework/Database-Practicum/Functions.cpp" "/home/petko/Workspace/FMI-Semester-2/Homework/Database-Practicum/cmake-build-debug/CMakeFiles/Database_Practicum.dir/Functions.cpp.o"
+  "/home/petko/Workspace/FMI-Semester-2/Homework/Database-Practicum/Student.cpp" "/home/petko/Workspace/FMI-Semester-2/Homework/Database-Practicum/cmake-build-debug/CMakeFiles/Database_Practicum.dir/Student.cpp.o"
+  "/home/petko/Workspace/FMI-Semester-2/Homework/Database-Practicum/main.cpp" "/home/petko/Workspace/FMI-Semester-2/Homework/Database-Practicum/cmake-build-debug/CMakeFiles/Database_Practicum.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Homework/Database-Practicum/cmake-build-debug/CMakeFiles/Database_Practicum.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Database_Practicum.dir/main.cpp.o"
+  "CMakeFiles/Database_Practicum.dir/Student.cpp.o"
+  "CMakeFiles/Database_Practicum.dir/Functions.cpp.o"
+  "Database_Practicum.pdb"
+  "Database_Practicum"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Database_Practicum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
